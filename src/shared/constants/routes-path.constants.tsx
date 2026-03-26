1fbd25103f30facc65551a8b849f94aacf496317
Tatiana Sergienko <tsergienko@nota.tech>
2026-03-26 18:16:12 +0600

    feat(routes): add routes for pagination and infinite pages

--- src/shared/constants/routes-path.constants.tsx
+++ src/shared/constants/routes-path.constants.tsx
@@ -1,3 +1,8 @@
+export const REPOSITORY_CHILD_ROUTES = {
+    PAGINATION: 'pagination',
+    INFINITE: 'infinite',
+} as const;
+
 export enum EAppRoutes {
     HOME = 'home',
     PROFILE = 'profile',
@@ -11,7 +16,7 @@ export const routePaths = {
     [EAppRoutes.HOME]: '/',
     [EAppRoutes.PROFILE]: '/profile/:login',
     [EAppRoutes.REPOSITORY]: '/repository',
-    [EAppRoutes.REPOSITORY_PAGINATION]: '/repository/pagination',
-    [EAppRoutes.REPOSITORY_INFINITE]: '/repository/infinite',
+    [EAppRoutes.REPOSITORY_PAGINATION]: `/repository/${REPOSITORY_CHILD_ROUTES.PAGINATION}`,
+    [EAppRoutes.REPOSITORY_INFINITE]: `/repository/${REPOSITORY_CHILD_ROUTES.INFINITE}`,
     [EAppRoutes.NOT_FOUND]: '*',
 } as const;
