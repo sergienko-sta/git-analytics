bd84fea37f5aabb00f6aa7a2acb0c5ba35cb0d96
sergienko-sta <sergienko.sta@gmail.com>
2026-03-27 20:17:40 +0600

    feature: add antd style lib (#38)
    * feat(styles): install antd style and create custom style for Card
    
    * fix(app): fix page svg icon
    
    * fix(widgets): fix code by FSD metodology
    
    ---------
    
    Co-authored-by: Tatiana Sergienko <tsergienko@nota.tech>

--- src/entities/repository/ui/repository-card/repository-card.component.tsx
+++ src/entities/repository/ui/repository-card/repository-card.component.tsx
@@ -2,25 +2,33 @@ import { Card, Space, Typography } from 'antd';
 
 import type * as Model from '../../model';
 
+import { useStyles } from './repository-card.styles';
+
 const { Text } = Typography;
 
 export const RepositoryCard = ({ repository, footer }: Model.IRepositoryCardProps) => {
+    const { styles } = useStyles();
     return (
         <Card
             key={repository.id}
+            className={styles.card}
             hoverable
             onClick={() => window.open(repository.html_url, '_blank', 'noopener noreferrer')}
         >
             <Card.Meta
                 title={
                     <Space orientation='vertical' size={0}>
-                        <Text strong>{repository.full_name}</Text>
+                        <Text strong className={styles.metaTitle}>
+                            {repository.full_name}
+                        </Text>
                     </Space>
                 }
                 description={
                     <>
                         {repository.description && (
-                            <Text type='secondary'>{repository.description}</Text>
+                            <Text type='secondary' className={styles.metaDescription}>
+                                {repository.description}
+                            </Text>
                         )}
                     </>
                 }
